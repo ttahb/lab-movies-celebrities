4efd52d5cf3c39f1d5ed0c304e73c8c69060cea5
ttahb <bhattvijay69@hotmail.com>
2023-12-16 02:56:45 +0530

    iteration 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,19 @@
+const Celebrity = require("../models/Celebrity.model");
+
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
 // all your routes here
+router.get('/movies/create', (req, res)=>{
+    Celebrity.find()
+        .then(celebrities=>{
+            res.render('movies/new-movie', {celebrities});
+        })
+        .catch(err=>next(err));
+});
+
+router.post('/movies/create', (req, res)=>{
+
+});
 
 module.exports = router;
