b9baf446df2f0cf4c39e47249a05f4b917627555
ttahb <bhattvijay69@hotmail.com>
2023-12-17 00:41:33 +0530

    iteration 9 10

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -33,18 +33,60 @@ router.get('/movies', (req,res, next)=>{
         })
 });
 
+//edit
+router.post('/movies/:id', (req, res)=>{
+    console.log('inside edit', req.body);
+    const { title, genre, plot, cast } = req.body;
+    Movie.findByIdAndUpdate(req.params.id, { title, genre, plot, cast }, {new:true})
+        .then(()=>res.redirect(`/movies/${req.params.id}`))
+        .catch(err => console.log('error updating the movie', err));
+});
+
 router.get('/movies/:id', (req, res, next)=>{
+    
     console.log('req.params', req.params);
     Movie.findById(req.params.id)
         .populate('cast')
         .then(movie=>{
             console.log('movie', movie);
-            res.render('movies/movie-details.hbs',{movie:movie});
+            res.render('movies/movie-details.hbs',movie);
         })
         .catch(err=>{
             console.log('err', err);
             next(err);
         })
 });
+
+router.post('/movies/:id/delete', (req, res, next)=>{
+    console.log('req.params', req.params);
+    Movie.findByIdAndRemove(req.params.id)
+        .then(()=> res.redirect('/movies'))
+        .catch(err => {
+            console.log(err);
+            next(err);
+        })
+});
+
+router.get('/movies/:id/edit', (req, res)=>{
+    console.log('entered edit', req.params);
+    Movie.findById(req.params.id)
+        .then(movie=> {
+            console.log('movies cast', movie.cast);
+            Celebrity.find()
+                .then(celebrities=> {
+                    res.render('movies/edit-movie.hbs', {movie, celebrities});
+                })
+                .catch(err=>{
+                    console.log('error while retrieving celebrities for edit', err);
+                })
+        })
+        .catch(err => {
+            console.log('error while retrieving movies for edit', err);
+        })
+});
+
+
+
+
 
 module.exports = router;
